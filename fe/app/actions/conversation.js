37c1ffa5380d9a1f3ffd457bfcbffdea06ce62d0
Radu Paraschiv <xpilot@gmail.com>
2016-05-27 10:08:31 -0400

    Add send message action - used for replying

--- fe/app/actions/conversation.js
+++ fe/app/actions/conversation.js
@@ -1,8 +1,19 @@
 import * as conversationApi from 'api/conversation';
+import * as messageApi from 'api/message';
 
 export const loadConversation = (id) => ({
   type: 'LOAD_CONVERSATION',
   payload: conversationApi.loadConversation(id)
+});
+
+export const sendMessage = (conversationId, messageBody, To, replyIn) => ({
+  type: 'POST_MESSAGE',
+  payload: messageApi.saveMessage({
+    body: messageBody,
+    conversationId,
+    replyIn,
+    to: To
+  })
 });
 
 export const setActiveMessage = (conversationId, messageId) => ({
