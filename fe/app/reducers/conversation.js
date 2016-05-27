bad4f03278c30e6fc6019687fc4d17089da0ce04
Radu Paraschiv <xpilot@gmail.com>
2016-05-27 10:09:40 -0400

    update conversation store to rock on and be able to navigate between messages

--- fe/app/reducers/conversation.js
+++ fe/app/reducers/conversation.js
@@ -9,24 +9,43 @@ const initialState = {
 export default function conversation(state = initialState, action) {
   switch (action.type) {
     case 'LOAD_CONVERSATION_SUCCESS':
-      return Object.assign({}, action.payload);
+      const newMessages = action.payload.messages.slice(0);
+      newMessages[0].active = true;
+      return Object.assign({}, action.payload, { messages: newMessages });
 
     case 'LOAD_CONVERSATION_FAIL':
       return Object.assign({}, initialState);
 
+
+    case 'POST_MESSAGE_SUCCESS':
+
+
+      const newPostMessages = action.payload;
+      newPostMessages[0].active = true;
+      return Object.assign({}, action.payload, { messages: newPostMessages });
+
+    case 'POST_MESSAGE_FAIL':
+      return Object.assign({}, initialState);
+
+
     case 'SET_ACTIVE_MESSAGE':
-      let messages = this.state.messages.slice(0);
-      messages = messages.map((item) => {
+      let currentMessages = state.messages.slice(0);
+      currentMessages = currentMessages.map(item => {
         const newItem = Object.assign({}, item);
+
         newItem.active = false;
 
-        if (newItem.id === action.payload.messageId) {
+        console.log(newItem.id, 'vs', action.payload.messageId);
+
+        if (action.payload.messageId === newItem.id) {
+          console.log('l-am gasit');
           newItem.active = true;
         }
+
         return newItem;
       });
 
-      return Object.assign({}, state, { messages });
+      return Object.assign({}, state, { messages: currentMessages });
 
     default:
       return state;
