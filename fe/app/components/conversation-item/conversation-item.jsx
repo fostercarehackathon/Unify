ce83024834bc42f97013df32041b2154fa6eb95d
Sabin Tudor <sabin@gmx.us>
2016-05-27 03:35:07 -0400

    [ConversationItem] Update structure & styling

--- fe/app/components/conversation-item/conversation-item.jsx
+++ fe/app/components/conversation-item/conversation-item.jsx
@@ -1,27 +1,55 @@
 // deps
 import React, { Component, PropTypes } from 'react';
-// import cx from 'classnames';
-// import moment from 'moment';
+import moment from 'moment';
+
+// components
+import { Grid, GridCell } from 'components/grid';
 
 // styles
 import './conversation-item.scss';
 
 export default class ConversationItem extends Component {
   static propTypes = {
-    message: PropTypes.object
+    message: PropTypes.object.isRequired
   };
 
-  // status
-  // from
-  // respond in
-  // message body
-  // date sent
+  getMessageStatusIcon(replyIn) {
+    if (replyIn > 0) {
+      return 'respond-later';
+    }
+
+    // default fallback
+    return 'respond-today';
+  }
 
   render() {
+    const { message } = this.props;
+    const messageIcon = this.getMessageStatusIcon(message.replyIn);
+
+    console.log('conversation message @ ', this.props.message);
+
     return (
-      <div className="ConversationItem">
-        conversation here
-      </div>
+      <Grid className="ConversationItem">
+        <GridCell className="ConversationItem-Status" col={1}>
+          {messageIcon}
+        </GridCell>
+
+        <GridCell className="ConversationItem-Sender" col={2}>
+          {message.from}
+        </GridCell>
+
+        <GridCell className="ConversationItem-ReplyIn" col={2}>
+          {(message.replyIn > 0) ? `In ${message.replyIn} days` : 'Today'}
+        </GridCell>
+
+        <GridCell className="ConversationItem-Subject" fit>
+          {message.subject}
+        </GridCell>
+
+        <GridCell className="ConversationItem-Date" col={1}>
+          {moment(message.date).format('MMM D, Y')}
+        </GridCell>
+      </Grid>
     );
   }
 }
