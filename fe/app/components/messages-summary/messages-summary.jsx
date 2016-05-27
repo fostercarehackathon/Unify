e2936896f1d62f75f2bcfaf120693518015219ee
Sabin Tudor <sabin@gmx.us>
2016-05-27 07:17:05 -0400

    [MessagesSummary] Update component structure (#wip)

--- fe/app/components/messages-summary/messages-summary.jsx
+++ fe/app/components/messages-summary/messages-summary.jsx
@@ -5,24 +5,57 @@ import cx from 'classnames';
 // styles
 import './messages-summary.scss';
 
-export default class ConversationItem extends Component {
+export default class MessagesSummary extends Component {
   static propTypes = {
+    items: PropTypes.arrayOf(PropTypes.shape({
+      type: PropTypes.string,
+      count: PropTypes.number
+    })),
+
+    onItemClick: PropTypes.func
   };
 
+  static defaultProps = {
+    items: [],
+    onItemClick: () => {}
+  };
+
+  getItems() {
+    const { items } = this.props;
+
+    // map items
+    const summaryItems = items.map((item, index) => {
+      const itemClassName = cx('MessagesSummary-Item', {
+        'MessagesSummary-lolozaur': true
+      });
+
+      return (
+        <li key={'summary-' + index} className={itemClassName}>
+          {item.type} <span className="MessagesSummary-Counter">{item.count}</span>
+        </li>
+      );
+    });
+
+    console.log('summary items@ ', summaryItems);
+
+    return summaryItems;
+  }
+
+  getTotalItems() {
+    return ~~(Math.random() * 100);
+  }
+
   render() {
-    // const conversationItemClasses = cx('ConversationItem', {
-    //   [`ConversationItem--${messageStatusIcon}`]: messageStatusIcon
-    // });
+    const summaryItems = this.getItems();
+    const totalItems = this.getTotalItems();
 
     return (
       <div className="MessagesSummary">
         <div className="MessagesSummary-TotalMessages">
-          Your messages <span className="MessagesSummary-Counter">26</span>
+          Your messages <span className="MessagesSummary-Counter">{totalItems}</span>
         </div>
         <ul>
-          <li>All <span className="MessagesSummary-Counter">1</span></li>
-          <li>Unread <span className="MessagesSummary-Counter">1</span></li>
-          <li>Read <span className="MessagesSummary-Counter">24</span></li>
+          {summaryItems}
         </ul>
       </div>
     );
