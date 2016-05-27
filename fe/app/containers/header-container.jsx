a2ece97c4e5f2f0b961e92809d2d453d853c93ff
Sabin Tudor <sabin@gmx.us>
2016-05-26 23:09:24 -0400

    Add MessageBar component to header container

--- fe/app/containers/header-container.jsx
+++ fe/app/containers/header-container.jsx
@@ -4,6 +4,7 @@ import React, { Component, PropTypes } from 'react';
 // components
 import RequestStatus from 'components/request-status';
 import UserBar from 'components/user-bar';
+import MessageBar from 'components/message-bar';
 
 class HeaderContainer extends Component {
   static propTypes = {
@@ -18,6 +19,7 @@ class HeaderContainer extends Component {
       <div>
         <RequestStatus status="success" />
         <UserBar username="Brandon" />
+        <MessageBar />
         header container here
       </div>
     );
