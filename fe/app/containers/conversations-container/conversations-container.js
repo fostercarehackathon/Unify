6db24bb0a965e78b4dce03e06303b6cc33cf4f26
Sabin Tudor <sabin@gmx.us>
2016-05-27 08:08:25 -0400

    Add summary store to Conversations container

--- fe/app/containers/conversations-container/conversations-container.js
+++ fe/app/containers/conversations-container/conversations-container.js
@@ -1,26 +1,36 @@
-import React, {Component, PropTypes} from 'react';
-import {asyncConnect} from 'redux-async-connect';
-import {connect} from 'react-redux';
+import React, { Component, PropTypes } from 'react';
+import { asyncConnect } from 'redux-async-connect';
+import { connect } from 'react-redux';
 
 import ConversationsPage from 'pages/conversations-page';
-import {loadConversations} from 'actions/conversations';
+
+// actions
+import { loadConversations } from 'actions/conversations';
+import { loadSummary } from 'actions/summary';
 
 @asyncConnect([{
-  promise: ({store: {dispatch}}) => {
-    return dispatch(loadConversations());
+  promise: ({ store: { dispatch }}) => {
+    return Promise.all([
+      dispatch(loadConversations()),
+      dispatch(loadSummary())
+    ]);
   }
 }])
-@connect(({conversations}) => ({
-  conversations: conversations.conversations
-}), ({loadConversations}))
+@connect((store) => ({
+  conversations: store.conversations.conversations,
+  summary: store.summary
+}), ({ loadConversations }))
 export default class ConversationsContainer extends Component {
   static propTypes = {
     conversations: PropTypes.array.isRequired,
+    summary: PropTypes.object.isRequired
   };
 
   render() {
+    console.log('??? SUMMARY @ ', this.props.summary);
+
     return (
-      <ConversationsPage {...this.props}/>
-    )
+      <ConversationsPage {...this.props} />
+    );
   }
 }
