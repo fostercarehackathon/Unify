123f7d875aa899644d2050cb7bba4d5bdaa977b9
Radu Paraschiv <xpilot@gmail.com>
2016-05-27 10:08:46 -0400

    Minor improvements to conversation message component

--- fe/app/components/conversation-message/conversation-message.jsx
+++ fe/app/components/conversation-message/conversation-message.jsx
@@ -1,5 +1,6 @@
 // deps
 import React, { Component, PropTypes } from 'react';
+import { browserHistory } from 'react-router';
 import autobind from 'autobind-decorator';
 import moment from 'moment';
 import cx from 'classnames';
@@ -8,11 +9,13 @@ import Button from 'components/button';
 import ReactQuill from 'react-quill';
 
 
+
 // styles
 import './conversation-message.scss';
 
 class ConversationMessage extends Component {
   static propTypes = {
+    conversationId: PropTypes.number,
     message: PropTypes.object,
     onClick: PropTypes.func,
     sendMessage: PropTypes.func
@@ -26,12 +29,19 @@ class ConversationMessage extends Component {
     };
   }
 
+  @autobind
+  onTextChange(val) {
+    this.setState({
+      editorState: val
+    });
+  }
+
   render() {
-    const { message, onClick, sendMessage } = this.props;
+    const { message, onClick, sendMessage, conversationId } = this.props;
 
     const { editorState } = this.state;
 
-    const { active, id, body, date, from } = message;
+    const { active, id, body, date, from, to } = message;
 
     return (
       <div className={
@@ -43,19 +53,24 @@ class ConversationMessage extends Component {
       >
         <ul className="ConversationMessage-Details" onClick={onClick.bind(this, id)}>
           <li className="CMD-From">{from.firstname} {from.lastname}</li>
-          <li className="CMD-Body">{body}</li>
+          <li className="CMD-Body" dangerouslySetInnerHTML={{ __html: body }} />
           <li className="CMD-ReceivedDate">
             {moment(date).startOf('hour').fromNow()} - {moment(date).format('HH:MM')}
           </li>
-          <li className="CMD-BodyLarge">{body}</li>
+          <li className="CMD-BodyLarge" dangerouslySetInnerHTML={{ __html: body }} />
         </ul>
 
-        <ReactQuill className="ConversationMessage-Editor" value={editorState} theme="snow" />
+        <ReactQuill
+          className="ConversationMessage-Editor"
+          onChange={this.onTextChange}
+          value={editorState}
+          theme="snow"
+        />
 
         <Button
           className="ConversationMessage-Submit"
           onClick={
-            sendMessage.bind(this, editorState)
+            sendMessage.bind(this, conversationId, editorState, to.username)
           }
         >
           Send
