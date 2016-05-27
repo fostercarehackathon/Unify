0f34e2ec36a5b849167260c687757825dec7076e
Sabin Tudor <sabin@gmx.us>
2016-05-27 08:06:08 -0400

    Update MessagesSummary component

--- fe/app/components/messages-summary/messages-summary.jsx
+++ fe/app/components/messages-summary/messages-summary.jsx
@@ -7,11 +7,7 @@ import './messages-summary.scss';
 
 export default class MessagesSummary extends Component {
   static propTypes = {
-    items: PropTypes.arrayOf(PropTypes.shape({
-      type: PropTypes.string,
-      count: PropTypes.number
-    })),
-
+    items: PropTypes.object.isRequired,
     onItemClick: PropTypes.func
   };
 
@@ -23,20 +19,22 @@ export default class MessagesSummary extends Component {
   getItems() {
     const { items } = this.props;
 
-    // map items
-    const summaryItems = items.map((item, index) => {
+    if (!Object.keys(items)) { return null; }
+
+    const summaryItems = Object.keys(items).map((item, index) => {
+      const itemType = item;
+      const itemCount = items[item].count;
+
       const itemClassName = cx('MessagesSummary-Item', {
         'MessagesSummary-lolozaur': true
       });
 
       return (
         <li key={'summary-' + index} className={itemClassName}>
-          {item.type} <span className="MessagesSummary-Counter">{item.count}</span>
+          {itemType} <span className="MessagesSummary-Counter">{itemCount}</span>
         </li>
       );
     });
-
-    console.log('summary items@ ', summaryItems);
 
     return summaryItems;
   }
