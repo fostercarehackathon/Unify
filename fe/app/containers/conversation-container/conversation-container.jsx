36fab98b27375f5289423ddc8f4758049170a396
Radu Paraschiv <xpilot@gmail.com>
2016-05-26 20:09:13 -0400

    Integrate store in conversation container

--- fe/app/containers/conversation-container/conversation-container.jsx
+++ fe/app/containers/conversation-container/conversation-container.jsx
@@ -1,9 +1,10 @@
 // deps
 import React, { Component, PropTypes } from 'react';
 import { asyncConnect } from 'redux-async-connect';
-import cx from 'classnames';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-import {loadConversation} from 'actions/conversation';
+import * as ConversationActions from 'actions/conversation';
 
 // components
 import Icon from 'components/icon';
@@ -17,11 +18,20 @@ import './conversation-container.scss';
 
 @asyncConnect([{
   promise: ({store: {dispatch}, params: {id}}) => {
-    return dispatch(loadConversation(id));
+    return dispatch(ConversationActions.loadConversation(id));
   }
 }])
+@connect(
+  (store) => ({
+    conversation: store.conversation
+  }),
+  (dispatch) => ({
+    actions: bindActionCreators(ConversationActions, dispatch)
+  })
+)
 class ConversationContainer extends Component {
   static propTypes = {
+    conversation: PropTypes.object,
     messages: PropTypes.array
   };
 
@@ -32,12 +42,18 @@ class ConversationContainer extends Component {
   }
 
   render() {
-    return (
-      <Overlay className="ConversationContainer" isOpen>
-        <div className="ConversationContainer-Header">
-          <Icon onClick={(ev) => console.log(ev)} name="chevron-left" />
-        </div>
+    const { id, lastMessage } = this.props.conversation;
 
+    return (
+      <Overlay className="ConversationContainer" isOpen={id ? true : false }>
+        <div className="ConversationContainer-Header">
+          <Icon
+            className="ConversationContainer-Icon"
+            onClick={(ev) => console.log(ev)}
+            name="chevron-left" />
+          <p className="ConversationContainer-ConversationDetails">Conversation Details</p>
+          <p className="ConversationContainer-LastMessage">Last message: {lastMessage}</p>
+        </div>
       </Overlay>
     );
   }
